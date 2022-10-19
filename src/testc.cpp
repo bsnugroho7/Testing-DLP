de7fb4542ea84f4a6a1e07e8ae0eaa3d31fdb7d9
bsnugroho7 <bagussatrionugroho@gmail.com>
2022-10-19 14:09:52 +0700

    cek

---
+++ src/testc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std(){
+    cout<<"Hello Tio";
+    cout<<"Forcepoint Test";
+    cout<<"Forhhhhpoint Test2";
+    cout<<"Forhhhhpoint Test2";
+    cout<<"Test";
+return 0;
+}
